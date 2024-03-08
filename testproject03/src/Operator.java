0daca34d77663d4935d14ed339850f1f93c8bc1b
jun-1256 <juns_1201@icloud.com>
2024-03-08 22:00:48 +0900

    2024/03/03

---
+++ testproject03/src/Operator.java
@@ -0,0 +1,14 @@
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// 四則演算、剰余
+		int a1 = 4 + 3;
+		System.out.println(a1 + "\n");
+		
+		int a2 = -8 - 3;
+		System.out.println(a2);
+
+	}
+
+}
---
+++ testproject03/src/Operator2.java
@@ -0,0 +1,14 @@
+
+public class Operator2 {
+
+	public static void main(String[] args) {
+		// 四則演算、剰余
+		var a3 = -5 * 3;
+		System.out.println(a3 + "\n");
+		
+		var a4 = 4 / 2;
+		System.out.println(a4);
+
+	}
+
+}
